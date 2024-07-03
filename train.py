59d8cd5b767a5e25864c23c469db648378ad2256
TanganHitam1 <mario.chamdjoko@mhs.itenas.ac.id>
2024-07-04 06:12:47 +0700

    update

--- train.py
+++ train.py
@@ -3,18 +3,15 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+
 from config import configurations
 from backbone.model_resnet import ResNet_50, ResNet_101, ResNet_152
 from backbone.model_irse import IR_50, IR_101, IR_152, IR_SE_50, IR_SE_101, IR_SE_152
 from head.metrics import ArcFace, CosFace, SphereFace, Am_softmax
 from loss.focal import FocalLoss
-from torch.utils.data import sampler, DataLoader
-# from util.utils import make_weights_for_balanced_classes, get_val_data, separate_irse_bn_paras, separate_resnet_bn_paras, warm_up_lr, schedule_lr, perform_val, get_time, buffer_val, AverageMeter, accuracy
-from util.utils import get_val_data, make_weights_for_balanced_classes, separate_irse_bn_paras, separate_resnet_bn_paras, warm_up_lr, schedule_lr, perform_val, get_time, AverageMeter, accuracy
-import matplotlib.pyplot as plt
-import numpy as np
-
-# from tensorboardX import SummaryWriter
+from util.utils import make_weights_for_balanced_classes, separate_irse_bn_paras, separate_resnet_bn_paras, warm_up_lr, schedule_lr, perform_val, get_time, buffer_val, AverageMeter, accuracy
+from data import dataset
+from tensorboardX import SummaryWriter
 from tqdm import tqdm
 import os
 
@@ -28,6 +25,7 @@ if __name__ == '__main__':
     torch.manual_seed(SEED)
 
     DATA_ROOT = cfg['DATA_ROOT'] # the parent root where your train/val/test data are stored
+    DATA_LIST_FILE = cfg['DATA_LIST_FILE']
     MODEL_ROOT = cfg['MODEL_ROOT'] # the root to buffer your checkpoints
     LOG_ROOT = cfg['LOG_ROOT'] # the root to log your train/val status
     BACKBONE_RESUME_ROOT = cfg['BACKBONE_RESUME_ROOT'] # the root to resume training from a saved checkpoint
@@ -59,7 +57,7 @@ if __name__ == '__main__':
     print(cfg)
     print("=" * 60)
 
-    # writer = SummaryWriter(LOG_ROOT) # writer for buffering intermedium results
+    writer = SummaryWriter(LOG_ROOT) # writer for buffering intermedium results
 
     train_transform = transforms.Compose([ # refer to https://pytorch.org/docs/stable/torchvision/transforms.html for more build-in online data augmentation
         transforms.Resize([int(128 * INPUT_SIZE[0] / 112), int(128 * INPUT_SIZE[0] / 112)]), # smaller side resized
@@ -69,24 +67,25 @@ if __name__ == '__main__':
         transforms.Normalize(mean = RGB_MEAN,
                             std = RGB_STD),
     ])
-    "./applications/align/"
-    dataset_train = datasets.ImageFolder("../Dataset loss/idol/", train_transform)
 
+    # dataset_train = datasets.ImageFolder(os.path.join(DATA_ROOT, 'imgs'), train_transform)
+    # dataset_train = dataset.Dataset(root = DATA_ROOT, data_list_file = DATA_LIST_FILE, phase = 'train', input_shape = INPUT_SIZE)
+    dataset_train = datasets.L
+    # datasets.
     # create a weighted random sampler to process imbalanced data
-    weights = make_weights_for_balanced_classes(dataset_train.imgs, len(dataset_train.classes))
+    weights = make_weights_for_balanced_classes(dataset_train, dataset_train.classes)
     weights = torch.DoubleTensor(weights)
-    sampler = sampler.WeightedRandomSampler(weights, len(weights))
+    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
 
-    train_loader = DataLoader(
+    train_loader = torch.utils.data.DataLoader(
         dataset_train, batch_size = BATCH_SIZE, sampler = sampler, pin_memory = PIN_MEMORY,
         num_workers = NUM_WORKERS, drop_last = DROP_LAST
     )
 
-    NUM_CLASS = len(train_loader.dataset.classes)
+    NUM_CLASS = train_loader.dataset.classes
     print("Number of Training Classes: {}".format(NUM_CLASS))
 
     # lfw, cfp_ff, cfp_fp, agedb, calfw, cplfw, vgg2_fp, lfw_issame, cfp_ff_issame, cfp_fp_issame, agedb_issame, calfw_issame, cplfw_issame, vgg2_fp_issame = get_val_data(DATA_ROOT)
-    lfw, lfw_issame = get_val_data(DATA_ROOT)
 
 
     #======= model & loss & optimizer =======#
@@ -101,7 +100,7 @@ if __name__ == '__main__':
                     'IR_SE_152': IR_SE_152(INPUT_SIZE)}
     BACKBONE = BACKBONE_DICT[BACKBONE_NAME]
     print("=" * 60)
-    print(BACKBONE)
+    # print(BACKBONE)
     print("{} Backbone Generated".format(BACKBONE_NAME))
     print("=" * 60)
 
@@ -158,20 +157,11 @@ if __name__ == '__main__':
 
 
     #======= train & validation & save checkpoint =======#
-    DISP_FREQ = len(train_loader) // 10 # frequency to display training loss & acc
+    DISP_FREQ = len(train_loader) // 100 # frequency to display training loss & acc
 
-    NUM_EPOCH_WARM_UP = NUM_EPOCH // 20  # use the first 1/25 epochs to warm up
+    NUM_EPOCH_WARM_UP = NUM_EPOCH // 25  # use the first 1/25 epochs to warm up
     NUM_BATCH_WARM_UP = len(train_loader) * NUM_EPOCH_WARM_UP  # use the first 1/25 epochs to warm up
     batch = 0  # batch index
-    best_acc = 0.0  # best accuracy
-
-    acc_list, loss_list = [], []  # buffer for visualization
-
-    fig, ax1 = plt.subplots()
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Accuracy', color = 'red')
-    ax2 = ax1.twinx()
-    ax2.set_ylabel('Loss', color = 'blue')
 
     for epoch in range(NUM_EPOCH): # start training process
 
@@ -227,24 +217,8 @@ if __name__ == '__main__':
         # training statistics per epoch (buffer for visualization)
         epoch_loss = losses.avg
         epoch_acc = top1.avg
-        acc_list.append(epoch_acc)
-        loss_list.append(epoch_loss)
-
-        plot_1 = ax1.plot(acc_list, color = 'red', label='Accuracy')
-        ax1.tick_params(axis ='y', labelcolor = 'red')
-        plot_2 = ax2.plot(loss_list, color = 'blue', label='Loss')
-        ax2.tick_params(axis ='y', labelcolor = 'blue')
-
-        lns = plot_1 + plot_2
-        labels = [l.get_label() for l in lns]
-        plt.legend(lns, labels, loc=0)
-
-        np.savetxt("acc_list.txt", acc_list)
-        np.savetxt("loss_list.txt", loss_list)
-        # plt.legend(["Accuracy", "Loss"], )
-        fig.savefig("train.png")
-        # writer.add_scalar("Training_Loss", epoch_loss, epoch + 1)
-        # writer.add_scalar("Training_Accuracy", epoch_acc, epoch + 1)
+        writer.add_scalar("Training_Loss", epoch_loss, epoch + 1)
+        writer.add_scalar("Training_Accuracy", epoch_acc, epoch + 1)
         print("=" * 60)
         print('Epoch: {}/{}\t'
             'Training Loss {loss.val:.4f} ({loss.avg:.4f})\t'
@@ -256,7 +230,7 @@ if __name__ == '__main__':
         # perform validation & save checkpoints per epoch
         # validation statistics per epoch (buffer for visualization)
         print("=" * 60)
-        # print("Perform Evaluation on LFW and Save Checkpoints...")
+        # print("Perform Evaluation on LFW, CFP_FF, CFP_FP, AgeDB, CALFW, CPLFW and VGG2_FP, and Save Checkpoints...")
         # accuracy_lfw, best_threshold_lfw, roc_curve_lfw = perform_val(MULTI_GPU, DEVICE, EMBEDDING_SIZE, BATCH_SIZE, BACKBONE, lfw, lfw_issame)
         # buffer_val(writer, "LFW", accuracy_lfw, best_threshold_lfw, roc_curve_lfw, epoch + 1)
         # accuracy_cfp_ff, best_threshold_cfp_ff, roc_curve_cfp_ff = perform_val(MULTI_GPU, DEVICE, EMBEDDING_SIZE, BATCH_SIZE, BACKBONE, cfp_ff, cfp_ff_issame)
@@ -272,16 +246,13 @@ if __name__ == '__main__':
         # accuracy_vgg2_fp, best_threshold_vgg2_fp, roc_curve_vgg2_fp = perform_val(MULTI_GPU, DEVICE, EMBEDDING_SIZE, BATCH_SIZE, BACKBONE, vgg2_fp, vgg2_fp_issame)
         # buffer_val(writer, "VGGFace2_FP", accuracy_vgg2_fp, best_threshold_vgg2_fp, roc_curve_vgg2_fp, epoch + 1)
         # print("Epoch {}/{}, Evaluation: LFW Acc: {}, CFP_FF Acc: {}, CFP_FP Acc: {}, AgeDB Acc: {}, CALFW Acc: {}, CPLFW Acc: {}, VGG2_FP Acc: {}".format(epoch + 1, NUM_EPOCH, accuracy_lfw, accuracy_cfp_ff, accuracy_cfp_fp, accuracy_agedb, accuracy_calfw, accuracy_cplfw, accuracy_vgg2_fp))
-        print("Epoch {}/{})".format(epoch + 1, NUM_EPOCH))
+        print("Epoch {}/{}".format(epoch + 1, NUM_EPOCH))
         print("=" * 60)
 
-        if epoch_acc > best_acc: # save best model
-            best_acc = epoch_acc
-            # if MULTI_GPU:
-            #     torch.save(BACKBONE.module.state_dict(), os.path.join(MODEL_ROOT, "Backbone_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(BACKBONE_NAME, epoch + 1, batch, get_time())))
-            #     torch.save(HEAD.state_dict(), os.path.join(MODEL_ROOT, "Head_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(HEAD_NAME, epoch + 1, batch, get_time())))
-            # else:
-            #     torch.save(BACKBONE.state_dict(), os.path.join(MODEL_ROOT, "Backbone_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(BACKBONE_NAME, epoch + 1, batch, get_time())))
-            #     torch.save(HEAD.state_dict(), os.path.join(MODEL_ROOT, "Head_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(HEAD_NAME, epoch + 1, batch, get_time())))
-            torch.save(BACKBONE.state_dict(), os.path.join(MODEL_ROOT, "Backbone_{}_Best.pth".format(BACKBONE_NAME)))
-            torch.save(HEAD.state_dict(), os.path.join(MODEL_ROOT, "Head_{}_Best.pth".format(HEAD_NAME)))
+        # save checkpoints per epoch
+        if MULTI_GPU:
+            torch.save(BACKBONE.module.state_dict(), os.path.join(MODEL_ROOT, "Backbone_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(BACKBONE_NAME, epoch + 1, batch, get_time())))
+            torch.save(HEAD.state_dict(), os.path.join(MODEL_ROOT, "Head_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(HEAD_NAME, epoch + 1, batch, get_time())))
+        else:
+            torch.save(BACKBONE.state_dict(), os.path.join(MODEL_ROOT, "Backbone_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(BACKBONE_NAME, epoch + 1, batch, get_time())))
+            torch.save(HEAD.state_dict(), os.path.join(MODEL_ROOT, "Head_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(HEAD_NAME, epoch + 1, batch, get_time())))
